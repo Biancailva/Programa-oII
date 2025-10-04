3bf59af8b95c8dcbfeb6ae2b6c46fd5df6d15dc3
Natalia Bianca <145494932+Biancailva@users.noreply.github.com>
2025-10-04 13:56:18 -0300

    Update Questao5.java

--- Listadequestoes/Questao5.java
+++ Listadequestoes/Questao5.java
@@ -3,19 +3,20 @@
 //números de 1 a 9 sem repetição, (b) cada coluna deve conter todos os números de 1 a 9 sem
 //repetição, (c) cada submatriz 3x3 deve conter todos os números de 1 a 9 sem repetição. Ao
 //final, informe se o Sudoku fornecido é válido ou inválido.
+import java.util.Scanner;
 public class  Questao5 {
     public static void main(String[] args) {
-        int[][] sudoku = {
-            {5, 3, 4, 6, 7, 8, 9, 1, 2},
-            {6, 7, 2, 1, 9, 5, 3, 4, 8},
-            {1, 9, 8, 3, 4, 2, 5, 6, 7},
-            {8, 5, 9, 7, 6, 1, 4, 2, 3},
-            {4, 2, 6, 8, 5, 3, 7, 9, 1},
-            {7, 1, 3, 9, 2, 4, 8, 5, 6},
-            {9, 6, 1, 5, 3, 7, 2, 8, 4},
-            {2, 8, 7, 4, 1, 9, 6, 3, 5},
-            {3, 4, 5, 2, 8, 6, 1, 7, 9}
-        };
+        int sudoku[][] = new int[9][9];
+        Scanner scan = new Scanner(System.in);
+        for(int i = 0; i < sudoku.length; i++){
+            System.out.println("Linha " + (i + 1));
+            for(int j = 0; j < sudoku[i].length; j++){
+                System.out.println("Digite o número da coluna " + (j + 1) + ": ");
+                sudoku[i][j] = scan.nextInt();
+            }
+        }
+        VerificarSudoku(sudoku, true, true, true);
+        
     }
         public static boolean verificarLinhas(int[][] sudoku) {
             for (int i = 0; i < 9; i++) {
