2fe0625305066ab868135a0a44f6b4d3e2b26fa5
Natalia Bianca <145494932+Biancailva@users.noreply.github.com>
2025-10-04 13:53:44 -0300

    Update PrimeiraQuestao.java

--- Listadequestoes/PrimeiraQuestao.java
+++ Listadequestoes/PrimeiraQuestao.java
@@ -1,4 +1,3 @@
-package br.edu.ifma.programacaoII.aulaI;
 //Leia as notas de 30 alunos. Implemente funções para calcular a média da turma, contar - ok
 //quantos alunos estão acima da média e exibir a maior e a menor nota. O programa deve
 //mostrar ao final todas essas estatísticas, permitindo uma análise do desempenho da turma.
@@ -7,18 +6,38 @@ import java.util.Scanner;
 public class PrimeiraQuestao {
     public static void main(String[] args) {
 
-            Scanner scan = new Scanner(System.in);
-            int alunos[][] = new int[30][3];
-            for (int i = 0; i < alunos.length; i++) {
-                System.out.println("Aluno " + (i + 1));
-                for (int j = 0; j < alunos[i].length; j++) {
-                    System.out.println("Digite a nota " + (j + 1) + ": ");
-                    alunos[i][j] = scan.nextInt();
-                }
+        Scanner scan = new Scanner(System.in);
+        int alunos[][] = new int[30][3];
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("Aluno " + (i + 1));
+            for (int j = 0; j < alunos[i].length; j++) {
+                System.out.println("Digite a nota " + (j + 1) + ": ");
+                alunos[i][j] = scan.nextInt();
             }
-            //resultados
-            //
+        }
+        AlunosAcimaMedia(alunos);
+        MaiorNota(alunos);
+        //MediaTurma(alunos);
+        //ImprimirResutados(alunos);
 
+        //resultados
+        //
+
+
+    }
+
+
+    public static void AlunosAcimaMedia(int[][] alunos) {
+        //mostrar quantos alunos estão acima da média
+        int somaNotas = 0;
+        for (int i = 0; i < alunos.length; i++) {
+            for (int j = 0; alunos[i].length > j; j++) {
+                somaNotas += alunos[i][j];
+            }
+        }
+    }
+
+    public static void MaiorNota(int[][] alunos) {
         int maiorNota = Integer.MIN_VALUE;
         int menorNota = Integer.MAX_VALUE;
         for (int i = 0; i < alunos.length; i++) {
@@ -31,13 +50,11 @@ public class PrimeiraQuestao {
                 }
             }
         }
-        //mostrar quantos alunos estão acima da média
-        int somaNotas = 0;
-        for (int i = 0; i < alunos.length; i++) {
-            for (int j = 0; alunos[i].length > j; j++) {
-                somaNotas += alunos[i][j];
-            }
-        }
+
+    }
+
+
+    public static void MediaTurma(double[][] alunos, int somaNotas) {
         double mediaTurma = (double) somaNotas / (alunos.length * alunos[0].length);
         int alunosAcimaMedia = 0;
         for (int i = 0; i < alunos.length; i++) {
@@ -50,8 +67,9 @@ public class PrimeiraQuestao {
                 alunosAcimaMedia++;
             }
         }
+    }
 
-
+    public static void ImprimirResutados(int [][] alunos, int maiorNota, int menorNota, double mediaTurma, int alunosAcimaMedia) {
         System.out.println("A maior nota da turma é: " + maiorNota);
         System.out.println("A menor nota da turma é: " + menorNota);
         System.out.println("A média da turma é: " + mediaTurma);
@@ -68,18 +86,7 @@ public class PrimeiraQuestao {
         }
 
     }
-
-
-
-}//modularizacao coloco todas as instancias na funcao, revisar void e quando devo usar ela, fazer isso amanha
-
-
-
-
-
-
-
-
-
+}
+          
 
 
