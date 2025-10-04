c6397621908c9badf455242901000eaf978fd23d
Natalia Bianca <145494932+Biancailva@users.noreply.github.com>
2025-10-04 13:24:27 -0300

    Update Questao2.java

--- Listadequestoes/Questao2.java
+++ Listadequestoes/Questao2.java
@@ -1,67 +1,117 @@
-//Implemente uma matriz 3x3 que simule o tabuleiro do jogo da velha. Dois jogadores
-//alternam suas jogadas (X e O). Após cada jogada, verifique se houve vencedor ou se o jogo
-//terminou em empate. O programa deve imprimir o tabuleiro a cada rodada e exibir uma
-//mensagem final indicando o resultado.
-public class Questao2 {
-    public static void main(String[] args) {
-        int tabuleiro[][] = new int[3][3];
-        //X = 1
-        //O = 2
-        //vazio = 0
-        //imprimir tabuleiro
-        for(int i = 0; i < tabuleiro.length; i++){
-            for(int j = 0; j < tabuleiro[i].length; j++){
-                System.out.print(tabuleiro[i][j] + " ");
-            }
-            System.out.println();
-        }
-        //jogadas
-        tabuleiro[0][0] = 1;
-        tabuleiro[0][1] = 2;
-        tabuleiro[0][2] = 1;
-        tabuleiro[1][0] = 2;
-        tabuleiro[1][1] = 1;
-        tabuleiro[1][2] = 2;
-        tabuleiro[2][0] = 1;
-        tabuleiro[2][1] = 2;
-        tabuleiro[2][2] = 1;
-        //imprimir tabuleiro
-        for(int i = 0; i < tabuleiro.length; i++){
-            for(int j = 0; j < tabuleiro[i].length; j++){
-                System.out.print(tabuleiro[i][j] + " ");
-            }
-            System.out.println();
-        }
-        //verificar vencedor
-        int vencedor = 0;
-        //verificar linhas
-        for(int i = 0; i < tabuleiro.length; i++){
-            if(tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]){
-                vencedor = tabuleiro[i][0];
-            }
-        }
-        //verificar colunas
-        for(int i = 0; i < tabuleiro.length; i++){
-            if(tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]){
-                vencedor = tabuleiro[0][i];
-            }
-        }
-        //verificar diagonais
-        if(tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]){
-            vencedor = tabuleiro[0][0];
-        }
-        if(tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]){
-            vencedor = tabuleiro[0][2];
-        }
-        //imprimir vencedor
-        if(vencedor == 1){
-            System.out.println("X venceu!");
-        }else if(vencedor == 2){
-            System.out.println("O venceu!");
-        }else{
-            System.out.println("Empate!");
-        }
+import java.util.Scanner;
 
+public class Questao2 {
+
+    public static final char VALOR_X = 'X'; //se usa char para definir valores padroes
+    public static final char VALOR_O = 'O';
+    public static final char VALOR_VAZIO = ' ';
+
+    public static void main(String[] args) {
+        //criar o tabuleiro - OK
+        //efetuar jogada - laço de repetiçao
+        //imprimir tabuleiro - laço de repetiçao - OK
+        //verificar se houve vencedor/empate - laço de repetiçao
+        //mensagem final mostrando resultado
+
+        Scanner input = new Scanner(System.in);
+        char[][] tabuleiro = new char[3][3];// aqui vai se iniciar o tabuleiro
+        inicializarTabuleiro(tabuleiro);
+        imprimirTabuleiro(tabuleiro);
+        char jogador = VALOR_X;
+
+        do {
+            System.out.println("Jogador " + jogador + " informe a posição da sua jogada.");
+            System.out.println("Linha [1 - 3]: ");
+            int linhaJogada = input.nextInt();
+            linhaJogada--; // se usa o sinal de menos para que se o jogador digitar 1 caia na coluna 0
+            System.out.println("Coluna [1 - 3]: ");
+            int colunaJogada = input.nextInt();
+            colunaJogada--;
+
+            boolean isJogadaBemSucedida = efetuarJogada(tabuleiro, linhaJogada, colunaJogada, jogador);
+            if (isJogadaBemSucedida) {
+                jogador = (jogador == VALOR_X) ? VALOR_O : VALOR_X;// trocando de jogador a cada partida
+            }
+            System.out.println();
+            imprimirTabuleiro(tabuleiro);
+        } while (!isPartidaFinalizou(tabuleiro));
 
     }
-}//modularizar e organizar o codigo em funcoes
+
+    public static boolean isPartidaFinalizou(char[][] tabuleiro) {
+        //verifica se houve vencedor
+        for (int i = 0; i < tabuleiro.length; i++) {
+            //verifica linhas
+            if (tabuleiro[i][0] != VALOR_VAZIO && tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) {
+                System.out.println("O jogador " + tabuleiro[i][0] + " venceu!");
+                return true;
+            }
+            //verifica colunas
+            if (tabuleiro[0][i] != VALOR_VAZIO && tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]) {
+                System.out.println("O jogador " + tabuleiro[0][i] + " venceu!");
+                return true;
+            }
+        }
+        //verifica diagonais
+        if (tabuleiro[0][0] != VALOR_VAZIO && tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) {
+            System.out.println("O jogador " + tabuleiro[0][0] + " venceu!");
+            return true;
+        }
+        if (tabuleiro[0][2] != VALOR_VAZIO && tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) {
+            System.out.println("O jogador " + tabuleiro[0][2] + " venceu!");
+            return true;
+        }
+        //verifica se houve empate
+        boolean isTabuleiroCheio = true;
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                if (tabuleiro[i][j] == VALOR_VAZIO) {
+                    isTabuleiroCheio = false;
+                    break;
+                }
+            }
+        }
+        if (isTabuleiroCheio) {
+            System.out.println("Empate!");
+            return true;
+        }
+        
+        return false;
+    }
+
+    public static boolean efetuarJogada(char[][] tabuleiro, int linha, int coluna, char jogador) {
+        //valida se as coordenadas estão dentro do limite [1 - 3]
+        boolean isLinhaInvalida = linha < 0 || linha > 2;
+        boolean isColunaInvalida = coluna < 0 || coluna > 2;
+        if (isLinhaInvalida || isColunaInvalida) {
+            System.out.println("Jogada inválida! Os valores das coordenadas não estão no limite esperado [1 - 3].");
+            return false;
+        }
+
+        //valida se a posiçao é válida (se está livre)
+        if (tabuleiro[linha][coluna] != VALOR_VAZIO) {
+            System.out.println("Jogada inválida! Posição já está marcada.");
+            return false;
+        }
+        tabuleiro[linha][coluna] = jogador;
+        return true;
+    }
+
+    public static void inicializarTabuleiro(char[][] tabuleiro) {
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                tabuleiro[i][j] = VALOR_VAZIO;
+            }
+        }
+    }
+
+    public static void imprimirTabuleiro(char[][] tabuleiro) {
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[i].length; j++) {
+                System.out.print("[" + tabuleiro[i][j] + "]");
+            }
+            System.out.println();
+        }
+    }
+
+}
