9de022fca7a5bdec94b28ae79d03b3d773937f6d
Natalia Bianca <145494932+Biancailva@users.noreply.github.com>
2025-11-13 19:03:01 -0300

    Add product search feature in Questao09
    Implement product search functionality using a HashMap.

--- api/Questao9.java
+++ api/Questao9.java
@@ -1,4 +1,27 @@
-package api;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-public class Questao9 {
+public class Questao09 {
+    public static void main(String[] args) {
+        // produto (key) --> quantidade (value)
+        Map<String, Integer> mapaProdutos = new HashMap<>();
+        mapaProdutos.put("placa de video", 5);
+        mapaProdutos.put("processador", 10);
+        mapaProdutos.put("memoria RAM", 3);
+        mapaProdutos.put("HD SSD", 5);
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite um produto para pesquisar no estoque: ");
+        String termoDeBusca = input.nextLine();
+
+        if (mapaProdutos.containsKey(termoDeBusca)) {
+            System.out.println("Produto encontrado: " + termoDeBusca + " qtd: " + mapaProdutos.get(termoDeBusca));
+        }
+        else {
+            System.out.println("Produto não encontrado!");
+        }
+
+    }
 }
